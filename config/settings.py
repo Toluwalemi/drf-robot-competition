b2405507262b98d0a7805c2603e6ac9c12ce85de
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-17 18:18:00 +0100

    feature: create custom pagination

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'api.helpers.custompagination.LimitOffsetPaginationWithUpperBound',
+    'PAGE_SIZE': 4
+}

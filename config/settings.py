f7488d6744c4b0289417bb933f54e68b51885381
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-18 20:09:36 +0100

    feature: update settings

--- config/settings.py
+++ config/settings.py
@@ -19,12 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0_dxj12th19^2!d0h4i)uj-*!^*vlmrs9gch35*blw@lc-+neb'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-0_dxj12th19^2!d0h4i)uj-*!^*vlmrs9gch35*blw@lc-+neb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ['robot-competition.herokuapp.com', '127.0.0.1']
 
 # Application definition
 

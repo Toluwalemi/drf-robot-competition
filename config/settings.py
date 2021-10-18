554fa29c8ff8cc1a62792eae01af3465015d186c
Toluwalemi <toluwalemisrael@gmail.com>
2021-10-18 20:50:57 +0100

    fix: change storage

--- config/settings.py
+++ config/settings.py
@@ -161,4 +161,4 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
